c50997e8cebc3fa240ee0167c793389d2ee6924f
Nicola R <nicola.rizzi89@hotmail.it>
2020-11-30 21:26:46 +0100

    Finish
    Navbar works fine.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,27 @@
-var mainFunzionalita = $('div.funz');
+var mainFunzionalita = $('div.funzionalita.funz__');
 var funzionalitaList = $('div.container-sottomenu');
+var abbonamenti = $('div.funzionalita.abb__');
+var funzioneAbbonamenti = $('div.container-sottomenu-abbonamenti')
+
 
 mainFunzionalita.mouseenter(function () { 
 funzionalitaList.show();
+funzioneAbbonamenti.hide();
 console.log(mainFunzionalita);
     
 });
 
 funzionalitaList.mouseleave(function () { 
 funzionalitaList.hide();
+
+});
+
+abbonamenti.mouseenter(function () {
+funzionalitaList.hide();
+funzioneAbbonamenti.show();
+});
+
+funzioneAbbonamenti.mouseleave(function () { 
+funzioneAbbonamenti.hide();
 
 });
